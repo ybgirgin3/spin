193536ed8dff47d4ef3a013f2a3199f5b43b0634
ybgirgin3 <ybgirgin3@gmail.com>
2021-06-20 17:14:39 +0300

    ilanlar anasayfaya eklendi

--- spin2/root/views.py
+++ spin2/root/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from posts.models import PostModel
 
 # Create your views here.
 def home(response):
-    return render(response, "root/home.html")
+    p = PostModel.objects.all()
+    return render(response, "root/home.html", {'posts': p})
+    # return render(response, "root/home.html")
+
+"""
+def list_all_view(response):
+    p = PostModel.objects.all()
+    return render(response, "posts/post_all.html", {'posts': p})
+"""
